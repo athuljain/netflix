d7fbfc41949ad84e6d72638891f93858a6b2e99e
athuljain <athuljainkj2@gmail.com>
2023-02-13 11:33:37 +0530

    athul

---
+++ netflix.js
@@ -0,0 +1,14 @@
+/*const accordion=document.querySelectorAll(".accordion")
+accordion.forEach(accordion => {
+    accordion.addEventListener("click",() =>{
+        accordion.classList.toggle("active");
+    })
+    
+});
+const accordion=document.getElementsByClassName('content');
+for(i=0;i<accordion.length;i++){
+    accordion[i].addEventListener('click',function(){
+        this.classList.toggle()
+
+    })
+}*/
